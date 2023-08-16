8449423dd28e72741684b33721618acd7aac75c3
fewtarius <fewtarius@jelos.org>
2023-08-16 16:41:52 +0000

    Fix 32bit audio.

--- packages/emulators/standalone/retroarch/package.mk
+++ packages/emulators/standalone/retroarch/package.mk
@@ -7,7 +7,7 @@ PKG_VERSION="62f3840e3883cf64c126c39b9668e6bb59a44606"
 PKG_SITE="https://github.com/libretro/RetroArch"
 PKG_URL="${PKG_SITE}.git"
 PKG_LICENSE="GPLv3"
-PKG_DEPENDS_TARGET="toolchain SDL2 alsa-lib libass openssl freetype zlib retroarch-assets core-info ffmpeg libass joyutils empty nss-mdns openal-soft libogg libvorbisidec libvorbis libvpx libpng libdrm pulseaudio miniupnpc flac"
+PKG_DEPENDS_TARGET="toolchain SDL2 alsa-lib libass openssl freetype zlib retroarch-assets core-info ffmpeg libass joyutils empty nss-mdns openal-soft libogg libvorbisidec libvorbis libvpx libpng libdrm pulseaudio pipewire miniupnpc flac"
 PKG_LONGDESC="Reference frontend for the libretro API."
 GET_HANDLER_SUPPORT="git"
 
